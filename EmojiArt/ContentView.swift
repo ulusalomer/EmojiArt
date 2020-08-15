7c5d55c0b2df9eceb4f29a202830c90f41d6cb91
Ömer Ulusal <ulusalomer@gmail.com>
2020-08-15 13:27:19 +0300

    Initial Commit

---
+++ EmojiArt/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  EmojiArt
+//
+//  Created by Ömer Ulusal on 15.08.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Ömer Ulusal on 15.08.2020.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
